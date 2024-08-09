f14efc2f6ff98de311473e74daf8a4b3dae5a85f
samliew94 <mlmsamliew@gmail.com>
2024-08-09 17:35:40 +0800

    updated dockerfile

--- apps/herd/user_data.sh
+++ apps/herd/user_data.sh
@@ -8,5 +8,4 @@ cd /
 git clone https://github.com/samliew94/open-board-games.git
 cd /open-board-games
 git switch v2.0.1-release
-cd /open-board-games/apps/herd
-docker build -t herd .
+docker build -t herd:latest /open-board-games/apps/herd
