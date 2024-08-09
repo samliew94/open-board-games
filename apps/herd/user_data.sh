2c003258b42fec7c4140755c89201d8194f9f4b0
samliew94 <root@LENOVOSAM>
2024-08-09 17:52:30 +0800

    updated user_data

--- apps/herd/user_data.sh
+++ apps/herd/user_data.sh
@@ -4,5 +4,5 @@ yum install -y git
 yum install -y docker
 systemctl enable docker
 systemctl start docker
-git clone -b v2.0.1-release https://github.com/samliew94/open-board-games.git ~
+git clone -b v2.0.1-release https://github.com/samliew94/open-board-games.git ~/open-board-games
 docker build -t herd:latest ~/open-board-games/apps/herd
